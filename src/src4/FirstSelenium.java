9a7e5135ab7a5f3b6b7c070ee32ed36e46d11ba2
OmarNavarro <onavarro@kueski.com>
2017-11-09 16:10:26 -0600

    Adds first Selenium example

---
+++ src/src4/FirstSelenium.java
@@ -0,0 +1,16 @@
+public class FirstSelenium {
+
+    public static void main(String[] args) {
+        WebDriver driver = new FirefoxDriver();
+        String expectedTitle = "Welcome: Mercury Tours"
+        driver.get("http://newtours.demoaut.com");
+        actualTitle = driver.getTitle();
+        if (actualTitle.contentEquals(expectedTitle)){
+            System.out.println("Test Passed!");
+        } else {
+            System.out.println("Test Failed");
+        }
+        driver.close();
+    }
+}
+
