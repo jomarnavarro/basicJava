21fe8cb9634adee6efe2e5b723eb10145cda548b
har6451 <68863245+har6451@users.noreply.github.com>
2020-10-02 09:36:48 +0545

    Update CharArrays.java

--- src/mArrays/CharArrays.java
+++ src/mArrays/CharArrays.java
@@ -5,8 +5,8 @@ public class CharArrays {
     public static void main(String args[]) {
         String s = new String ("ABCDEFG") ;
         char[] a = s.toCharArray();
-        System.out.println("s = \"" + s + "\"\t\ta = \"" + a + "\"");
-        System.out.println("s.lenght() = " + s.length() + "\t\ta.length = " + a.length);
+        System.out.println("s = \"" + s);
+        System.out.println("s.length() = " + s.length() + "\t\ta.length = " + a.length);
 
         for(int i = 0; i < a.length; i++) {
             System.out.println("s.charAt(" + i + ") = " + s.charAt(i) + "\t\ta[" + i + "] = " + a[i]);
